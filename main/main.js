04ab42c01bf903101dbfcee09da4193110481dcc
zhuyuezy <2595223212@qq.com>
2019-05-21 17:15:39 +0800

    1

--- main/main.js
+++ main/main.js
@@ -1,4 +1,36 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(number) {
+	number = String(number);
+	var check = /^\d+$/;
+    if (!check.test(number))
+    {
+    	return;
+    }
+    
+    var LCDarray = new Array;
+    LCDarray[0] = ['._.', '|.|', '|_|']; //0 
+    LCDarray[1] = ['...', '..|', '..|'];
+    LCDarray[2] = ['._.', '._|', '|_.'];
+    LCDarray[3] = ['._.', '._|', '._|'];
+    LCDarray[4] = ['...', '|_|', '..|'];
+    LCDarray[5] = ['._.', '|_.', '._|'];
+    LCDarray[6] = ['._.', '|_.', '|_|'];
+    LCDarray[7] = ['._.', '..|', '..|'];
+    LCDarray[8] = ['._.', '|_|', '|_|'];
+    LCDarray[9] = ['._.', '|_|', '..|'];
+
+    var result = '';
+
+   	for(var row=0; row<3; row++)
+   	{
+    	for(var i=0; i<number.length; i++)
+    	{
+    		result = result + LCDarray[number[i]][row] + ' ';
+   		}
+
+   		result = result.substr(0,result.length-1);
+   		result = result + '\n';
+    }
+
+    console.log(result);
+    return result;
 };
